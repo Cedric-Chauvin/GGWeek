2f427322c8c5bbf0ebf2775959a1247b326d10dc
Abdelkader VANDAMME <vandammeabdel@gmail.com>
2019-05-08 14:48:31 +0200

    Barriere

---
+++ Unity/Assets/Script/Shield.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    public SpriteRenderer shield;
+    public Collider2D shieldCollider;
+    private bool visible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shield = GetComponent<SpriteRenderer>();
+        shieldCollider = GetComponent<Collider2D>();
+        visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            shield.enabled = !shield.enabled;
+            shieldCollider.enabled = !shieldCollider.enabled;
+        }
+    }
+}
