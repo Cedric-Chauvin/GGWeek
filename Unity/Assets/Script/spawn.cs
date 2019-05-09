406a43255452cf9e820174e886f777a323652516
Abdelkader VANDAMME <vandammeabdel@gmail.com>
2019-05-09 14:09:59 +0200

    Inté. assets

---
+++ Unity/Assets/Script/spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+
+    public float rate;
+    public GameObject[] mongol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", rate, rate);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        Instantiate(mongol[(int)Random.Range(0, mongol.Length)], new Vector3(Random.Range(14.5f, 14.5f), 4, -1), Quaternion.identity);
+    }
+}
