22ef0ddcf7d03e7dc645edf6eb3c33f1d89f4a53
Abdelkader VANDAMME <vandammeabdel@gmail.com>
2019-05-10 09:28:25 +0200

    montgolfière et brouillard

---
+++ Unity/Assets/Script/Smog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smog : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float xSpeed;
+    public float ySpeed;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(xSpeed, ySpeed * -1);
+    }
+}
